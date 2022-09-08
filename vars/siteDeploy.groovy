ef899115575bbb880dc56d14937eb77841d7b7d4
Sandeep <get.rajput@outlook.com>
2022-09-08 22:41:32 +1000

    minor change

--- vars/siteDeploy.groovy
+++ vars/siteDeploy.groovy
@@ -5,31 +5,32 @@ def call(Map config) {
 
         agent { label "${config.build.image}" }
        
+       stages {
+            stage('Build') { 
 
-        stage('Build') { 
-
-            steps {
-                script {
-                echo 'Building...'
+                steps {
+                    script {
+                    echo 'Building...'
+                    }
                 }
             }
-        }
 
-        stage('Unit Test') {
-            steps {
-                script {
-                echo 'Unit Testing...'
+            stage('Unit Test') {
+                steps {
+                    script {
+                    echo 'Unit Testing...'
+                    }
                 }
             }
-        }
 
-        stage('Deploy') {
-            steps {
-                script {
-                echo 'Deploying...'
+            stage('Deploy') {
+                steps {
+                    script {
+                    echo 'Deploying...'
+                    }
                 }
             }
-        }
+       }
 
     }
 
